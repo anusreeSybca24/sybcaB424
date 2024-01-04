90bd8169ff6a7bfc3bb4fda0f5dddbb58195c294
AnuCasetools <anusree@rosarycollege.org>
2024-01-04 09:38:49 +0530

    commit first

---
+++ SybcaB4project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author admin
+ *
+ */
+module SybcaB4project {
+}
---
+++ SybcaB4project/src/pkgB4Sybca/B4DemoClass.java
@@ -0,0 +1,10 @@
+package pkgB4Sybca;
+
+public class B4DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Github Hello world");
+	}
+
+}
